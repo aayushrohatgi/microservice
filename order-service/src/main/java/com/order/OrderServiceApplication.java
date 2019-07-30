cb3a67b0751f04a25f2ff6b34ab2e4f1fbad193c
Kartik Arora <kartik.arora@nagarro.com>
2019-07-30 16:21:29 +0530

    Changes

--- order-service/src/main/java/com/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/order/OrderServiceApplication.java
@@ -3,19 +3,18 @@ package com.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import com.order.service.OrderService;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
-		OrderService orderService = new OrderService();
-		orderService.createOrder();
 	}
 	
 	@Bean
