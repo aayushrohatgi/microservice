93b13684752bc553a45b33df993da32e983242aa
Kartik Arora <kartik.arora@nagarro.com>
2019-07-31 16:03:24 +0530

    Integrated spring boot email sender with Notification Service

--- notification-service/src/main/java/com/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/notification/service/NotificationService.java
@@ -1,13 +1,26 @@
 package com.notification.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.notification.dto.NotificationData;
 
 @Service
 public class NotificationService {
+	
+	@Autowired
+    private JavaMailSender javaMailSender;
 
 	public void sendEmailNotification(NotificationData notificationDto) {
+		SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(notificationDto.getEmail());
+        msg.setFrom(notificationDto.getEmail());
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText(notificationDto.getContent());
+
+        javaMailSender.send(msg);
 		System.out.println("Email notification Sent to"+ notificationDto.getEmail());
 	}
 	
